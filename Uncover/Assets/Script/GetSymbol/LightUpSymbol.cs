22e1c4fc41e47f67643064f3349012b18f4faf9a
paledust <paledust@hotmail.com>
2016-12-04 21:55:27 -0500

    Change only little things

---
+++ Uncover/Assets/Script/GetSymbol/LightUpSymbol.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightUpSymbol : MonoBehaviour {
+    public string symbolReceive;
+    public string[] symbolLight;
+
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
