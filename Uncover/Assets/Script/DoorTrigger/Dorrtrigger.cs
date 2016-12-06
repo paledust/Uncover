3168b8413bac1e51ac39f1ae32e3ba6055ccb858
paledust <paledust@hotmail.com>
2016-12-06 01:49:18 -0500

    Editing script for changing level

--- Uncover/Assets/Script/DoorTrigger/Dorrtrigger.cs
+++ Uncover/Assets/Script/DoorTrigger/Dorrtrigger.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 
 public class Dorrtrigger : MonoBehaviour {
     public Transform endPoint;
+    public Transform respawnPos;
 
     public GameObject player;
     public Image BackGround;
@@ -12,10 +13,13 @@ public class Dorrtrigger : MonoBehaviour {
 
     private bool ifActive;
 
+    private bool stage_1;
+
     void Start()
     {
         ifActive = false;
         BackGround.enabled = false;
+        stage_1 = false;
     }
 
     void OnTriggerEnter(Collider collider)
@@ -33,6 +37,11 @@ public class Dorrtrigger : MonoBehaviour {
         player.transform.position = Vector3.Lerp(player.transform.position, endPoint.position, 0.3f * Time.deltaTime);
     }
 
+    void RespawnPlayer()
+    {
+        player.transform.position = respawnPos.position;
+    }
+
     void DarkScene()
     {
         if (!BackGround.enabled)
@@ -45,12 +54,29 @@ public class Dorrtrigger : MonoBehaviour {
         }
     }
 
+    void LightScene()
+    {
+        BackGround.color = Color.Lerp(BackGround.color, Color.clear, Time.deltaTime * fadeSpeed);
+
+        if (BackGround.color.a <= 0.03)
+        {
+            BackGround.color = Color.clear;
+            BackGround.enabled = false;
+            gameObject.SetActive(false);
+        }
+    }
+
     void Update()
     {
         if (ifActive)
         {
             Invoke("DarkScene", 5.0f*Time.deltaTime);
             MoveCharacter();
+        }
+
+        if (BackGround.color == fadeColor && !stage_1)
+        {
+
         }
     }
 }
